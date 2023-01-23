5ee71e210a56cb1434315689a9e4ee89131a1c01
michaelaguilar89 <mchlaguilar051@hotmail.es>
2023-01-23 10:21:23 -0600

    agregando por injeccion de dependencias un service que conecte mi DbContext con la cadena de conexion en appSettings.json

--- Program.cs
+++ Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using WebApi_Control_Production.Connection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>//usamos injeccion de dependencias hago referencia 
+{														//a DbContext y conecto con mi cadena de conexion
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
