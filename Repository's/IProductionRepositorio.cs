d88ea5c9611c2b14b0fc3fd59097942a665a2ab2
michaelaguilar89 <mchlaguilar051@hotmail.es>
2023-01-23 10:57:21 -0600

    agregando interface Production

---
+++ Repository's/IProductionRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using WebApi_Control_Production.Models;
+
+namespace WebApi_Control_Production.Repository_s
+{
+	public interface IProductionRepositorio
+	{
+		Task<List<Production>> GetProductions(DateTime date);
+
+		Task<Production> GetProductionById(int id);
+
+		Task<string> CreateUpdate(Production prod);
+
+		Task<string> Delete(int id);
+
+
+	}
+}
