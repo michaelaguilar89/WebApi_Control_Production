a9d44bb30352815a1ff19ef0a6014a551f8d3da2
michaelaguilar89 <mchlaguilar051@hotmail.es>
2023-01-23 10:59:08 -0600

    creando productionRepository

---
+++ Repository's/ProductionRepository.cs
@@ -0,0 +1,27 @@
+﻿using WebApi_Control_Production.Models;
+
+namespace WebApi_Control_Production.Repository_s
+{
+	public class ProductionRepository : IProductionRepositorio
+	{
+		public Task<string> CreateUpdate(Production prod)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<string> Delete(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Production> GetProductionById(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<List<Production>> GetProductions(DateTime date)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
