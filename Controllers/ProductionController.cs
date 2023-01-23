c88d5f45b0c582f185a4bd115a39332e273460e0
michaelaguilar89 <mchlaguilar051@hotmail.es>
2023-01-23 12:08:42 -0600

    configurando e Crud en el controller

--- Controllers/ProductionController.cs
+++ Controllers/ProductionController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebApi_Control_Production.Models;
+using WebApi_Control_Production.Repository_s;
+using WebApiProduccion.Models;
 
 namespace WebApi_Control_Production.Controllers
 {
@@ -7,5 +10,128 @@ namespace WebApi_Control_Production.Controllers
 	[ApiController]
 	public class ProductionController : ControllerBase
 	{
+		private readonly IProductionRepositorio _production;
+		protected myResponse _response;
+		public ProductionController(IProductionRepositorio production)
+		{
+			_production = production;
+			_response = new myResponse();
+		}
+		[HttpGet("{id}")]
+		public async Task< ActionResult> GetProductionById(int id)
+		{
+			try
+			{
+				Production model = await _production.GetProductionById(id);
+				if (model ==null)
+				{
+					_response.DisplayMessage = "El registro consultado no existe";
+					return Ok(_response); 
+				}
+				_response.Result = model;
+				_response.DisplayMessage = "Registro Id : " + id;
+				return Ok(_response);
+			}
+			catch (Exception e)
+			{
+				_response.DisplayMessage = "Error al obtener el registro Id : "+id;
+				_response.ErrorMessages = new List<string> { e.ToString() };
+				return BadRequest(_response);
+			}
+		}
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<Production>>> GetProduction(DateTime date)
+		{
+			try
+			{
+				var list = await _production.GetProductions(date);
+				_response.Result = list;
+				_response.DisplayMessage = "List of " + date;
+				return Ok(_response);
+			}
+			catch (Exception e)
+			{
+
+				_response.ErrorMessages = new List<string> { e.ToString() };
+				return BadRequest(_response);
+			}
+		}
+
+		[HttpPost]
+		public async Task<ActionResult<Production>> CreateProduction(Production model)
+		{
+			try
+			{
+				string mensaje;
+				mensaje = await _production.CreateUpdate(model);
+				if (mensaje== "create")
+				{
+					_response.Result = model;
+					_response.DisplayMessage = "Registro Creado";
+					return CreatedAtAction("GetCliente", new { Id = model.id }, _response);
+				}
+				_response.DisplayMessage = "Error interno";
+				return BadRequest(_response);
+			}
+			catch (Exception e)
+			{
+				_response.DisplayMessage = "Error al grabar el registro";
+				_response.ErrorMessages = new List<string> { e.ToString() };
+				return BadRequest(_response);
+			}
+		}
+		[HttpPut]
+		public async Task<ActionResult> EditProduction(Production model)
+		{
+			try
+			{
+				string mensaje;
+				mensaje = await _production.CreateUpdate(model);
+				if (mensaje == "update")
+				{
+					_response.DisplayMessage = "Record Actualizado";
+					_response.Result = model;
+					return Ok(_response);
+				}
+				_response.DisplayMessage = "Error interno ";
+				return BadRequest(_response);
+			}
+			catch (Exception e)
+			{
+				_response.DisplayMessage = "Error al actaulizar el registro";
+				_response.ErrorMessages = new List<string> { e.ToString() };
+				return BadRequest(_response);
+			}
+		}
+		[HttpDelete("{id}")]
+		public async Task<ActionResult> DeleteProduction(int id)
+		{
+			try
+			{
+				string mensaje = await _production.Delete(id);
+				if (mensaje== "delete")
+				{
+					_response.DisplayMessage = "Record Eliminando";
+					return Ok(_response);
+				}
+				else
+				{
+					if (mensaje== "El record no exixte")
+					{
+						_response.DisplayMessage = "El record no existe";
+						return BadRequest(_response);
+					} 
+				}
+				_response.DisplayMessage = "Error Interno";
+				return BadRequest(_response);
+			}
+			catch (Exception e)
+			{
+				_response.DisplayMessage = "Error al eliminar el registro";
+				_response.ErrorMessages = new List<string> { e.ToString() };
+				return BadRequest(_response);
+			}
+		}
+		
 	}
 }
