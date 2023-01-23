e72873a5218ec9e8d7b1ef032919035c2167b34c
michaelaguilar89 <mchlaguilar051@hotmail.es>
2023-01-23 10:12:01 -0600

    configurando DbContext

---
+++ Connection/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi_Control_Production.Models;
+
+namespace WebApi_Control_Production.Connection
+{
+	public class ApplicationDbContext:DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+		{
+		
+		}
+
+		public DbSet<Production> productions { get; set; }	
+	}
+}
