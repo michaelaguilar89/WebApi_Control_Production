a4327be706b1945e30cbf684fbb3e85464ef2092
michaelaguilar89 <mchlaguilar051@hotmail.es>
2023-01-25 19:15:54 -0600

    agregando class LambdaFuntion

---
+++ aws/LambdaFunction.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApi_Control_Production.aws
+{
+	public class LambdaFunction : Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
+	{
+		protected override void Init(IWebHostBuilder builder)
+		{
+			builder.UseContentRoot(Directory.GetCurrentDirectory())
+				.UseLambdaServer()
+				.UseApiGateway();
+		}
+	}
+}
